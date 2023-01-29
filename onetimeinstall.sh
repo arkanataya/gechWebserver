d86212c42c3a53b883c58695f65a5722101eb5c8
arkanataya <78206974+arkanataya@users.noreply.github.com>
2023-01-29 12:31:10 +0700

    Update onetimeinstall.sh

--- onetimeinstall.sh
+++ onetimeinstall.sh
@@ -1,9 +1,10 @@
-if ["$(uname)" == "Linux"];
+if [ "$(uname)" == "Linux" ];
  then
 apt install python3 pip3 -y
 apt install toilet -y
 else
 echo "E: Os Not Supported";
+exit 1
 fi
 toilet GechWebServer
 echo Timer System install a some package in the geba server
